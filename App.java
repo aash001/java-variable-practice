6a01e765ea16ab1303b610a5f49b43ea4ebb1b35
aampinar <98187277+aampinar@users.noreply.github.com>
2022-05-24 13:47:26 -0400

    Add solution

--- App.java
+++ App.java
@@ -1,6 +1,19 @@
 class App {
-  public static void main(String args[]){
-    String message = "Hello, world!";
-    System.out.println(message);
+  public static void main(String args[]) {
+    int vin = 1000852;
+    String makeModel = "Toyota Corolla";
+    String color = "Blue";
+    boolean towingPackage = true;
+    int odometer = 29850;
+    double price = 2000;
+    String qualityRating = "A";
+
+    System.out.println(vin);
+    System.out.println(makeModel);
+    System.out.println(color);
+    System.out.println(towingPackage);
+    System.out.println(odometer);
+    System.out.println(price);
+    System.out.println(qualityRating);
   }
 }
